ec86bd1249e920d40b0cd7f7e3ec62dd0673da18
sonyohann <sonyohann@naver.com>
2021-12-03 14:21:58 +0900

    12/03

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import React, { useRef, useState } from "react";
-import CreateUser from "./CreateUser";
+import React, { useRef, useState, useMemo, useCallback } from "react";
 import UserList from "./UserList";
+import CreateUser from "./CreateUser";
+
+function countActiveUsers(users) {
+  console.log("활성 사용자 수를 세는중...");
+  return users.filter((user) => user.active).length;
+}
 
 function App() {
   const [inputs, setInputs] = useState({
     username: "",
     email: "",
   });
-
   const { username, email } = inputs;
-
-  const onChange = (e) => {
+  const onChange = useCallback((e) => {
     const { name, value } = e.target;
-    setInputs({
+    setInputs((inputs) => ({
       ...inputs,
       [name]: value,
-    });
-  };
+    }));
+  }, []);
   const [users, setUsers] = useState([
     {
       id: 1,
@@ -39,33 +42,32 @@ function App() {
   ]);
 
   const nextId = useRef(4);
-
-  const onCreate = () => {
+  const onCreate = useCallback(() => {
     const user = {
       id: nextId.current,
       username,
       email,
     };
-    setUsers(users.concat(user));
+    setUsers((users) => users.concat(user));
+
     setInputs({
       username: "",
       email: "",
     });
     nextId.current += 1;
-  };
+  }, [username, email]);
 
-  const onRemove = (id) => {
-    setUsers(users.filter((user) => user.id !== id));
-  };
-
-  const onToggle = (id) => {
-    setUsers(
+  const onRemove = useCallback((id) => {
+    setUsers((users) => users.filter((user) => user.id !== id));
+  }, []);
+  const onToggle = useCallback((id) => {
+    setUsers((users) =>
       users.map((user) =>
         user.id === id ? { ...user, active: !user.active } : user
       )
     );
-  };
-
+  }, []);
+  const count = useMemo(() => countActiveUsers(users), [users]);
   return (
     <>
       <CreateUser
@@ -75,6 +77,7 @@ function App() {
         onCreate={onCreate}
       />
       <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
+      <div>활성사용자 수 : {count}</div>
     </>
   );
 }
