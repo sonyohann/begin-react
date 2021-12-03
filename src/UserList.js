7327858f93d48aafdbf28e82f46142d0f04d14fe
sonyohann <sonyohann@naver.com>
2021-12-03 10:31:59 +0900

    12/03

--- src/UserList.js
+++ src/UserList.js
@@ -1,20 +1,32 @@
 import React, { useRef } from "react";
 
-function User({ user, onRemove }) {
-  const { username, email, id } = user;
+function User({ user, onRemove, onToggle }) {
+  const { username, email, id, active } = user;
   return (
     <div>
-      <b>{user.username}</b> <span>({user.email})</span>
+      <b
+        style={{ color: active ? "green" : "black", cursor: "pointer" }}
+        onClick={() => onToggle(id)}
+      >
+        {username}
+      </b>
+      &nbsp;
+      <span>({email})</span>
       <button onClick={() => onRemove(id)}>삭제</button>
     </div>
   );
 }
 
-function UserList({ users, onRemove }) {
+function UserList({ users, onRemove, onToggle }) {
   return (
     <div>
       {users.map((user) => (
-        <User user={user} key={user.id} onRemove={onRemove} />
+        <User
+          user={user}
+          key={user.id}
+          onRemove={onRemove}
+          onToggle={onToggle}
+        />
       ))}
     </div>
   );
